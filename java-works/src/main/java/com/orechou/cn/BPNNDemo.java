aa99d31cefcada371ad1a3ce05e351d49edeab26
OreChou <orechou1018@gmail.com>
2018-03-07 11:10:56 +0800

    增加Java工作目录

---
+++ java-works/src/main/java/com/orechou/cn/BPNNDemo.java
@@ -0,0 +1,73 @@
+package com.orechou.cn;
+
+import org.deeplearning4j.nn.api.OptimizationAlgorithm;
+import org.deeplearning4j.nn.conf.MultiLayerConfiguration;
+import org.deeplearning4j.nn.conf.NeuralNetConfiguration;
+import org.deeplearning4j.nn.conf.Updater;
+import org.deeplearning4j.nn.conf.layers.DenseLayer;
+import org.deeplearning4j.nn.conf.layers.OutputLayer;
+import org.deeplearning4j.nn.multilayer.MultiLayerNetwork;
+import org.deeplearning4j.nn.weights.WeightInit;
+import org.deeplearning4j.optimize.listeners.ScoreIterationListener;
+import org.nd4j.linalg.activations.Activation;
+import org.nd4j.linalg.api.ndarray.INDArray;
+import org.nd4j.linalg.dataset.DataSet;
+import org.nd4j.linalg.factory.Nd4j;
+import org.nd4j.linalg.lossfunctions.LossFunctions;
+
+/**
+ * @author OreChou
+ * @create 2018-03-07 10:25
+ */
+public class BPNNDemo {
+
+    public static int rngSeed = 120;
+    public static int iterations = 10;
+
+    public static int inNumbers = 21;
+    public static int outNumbers = 6;
+
+
+    public static void main(String[] args) {
+
+        INDArray indArray = Nd4j.rand(1000,21);
+        System.out.println(indArray);
+
+
+
+        MultiLayerConfiguration conf = new NeuralNetConfiguration.Builder()
+                .seed(rngSeed)
+                .iterations(iterations)
+                .optimizationAlgo(OptimizationAlgorithm.STOCHASTIC_GRADIENT_DESCENT)
+                .learningRate(0.006)
+                .updater(Updater.NESTEROVS)
+                .regularization(true).l2(1e-4)
+                .list()
+                .layer(0, new DenseLayer.Builder()
+                        .nIn(inNumbers)
+                        .nOut(1000)
+                        .activation(Activation.RELU)
+                        .weightInit(WeightInit.XAVIER)
+                        .build())
+                .layer(1, new OutputLayer.Builder(LossFunctions.LossFunction.NEGATIVELOGLIKELIHOOD)
+                        .nIn(1000)
+                        .nOut(outNumbers)
+                        .activation(Activation.SOFTMAX)
+                        .weightInit(WeightInit.XAVIER)
+                        .build())
+                .pretrain(false)
+                .backprop(true)
+                .build();
+
+
+        MultiLayerNetwork model = new MultiLayerNetwork(conf);
+        model.init();
+        //print the score with every 1 iteration
+        model.setListeners(new ScoreIterationListener(1));
+
+
+
+
+    }
+
+}
